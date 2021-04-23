c943e0c1024cbaf7cc500c887a5280c2afa7e0c7
ecspetra <ecspetra@gmail.com>
2021-04-23 18:37:44 +0300

    start_example

---
+++ js/slider-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".hero-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+  });
+});
